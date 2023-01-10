87652a1b78fb5f6379ccd87ceccf1b4de3949341
Rick Kauffman <rick@rickkauffman.com>
2023-01-09 18:30:11 -0700

    init6

--- actions/lib/actions.py
+++ actions/lib/actions.py
@@ -58,7 +58,7 @@ class ArubaCentralBaseAction(Action):
         }
         '''
         ssl_verify = True
-        central_info = json.dumps(central_info)
+        central_info = json.loads(central_info)
         print(central_info)
         print(type(central_info))
         '''
